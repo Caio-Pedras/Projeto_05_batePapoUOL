147bdf1e4ea1ab9c458443f0d4bc8004c8f7504e
CaioPedras <caio.vp@gmail.com>
2022-04-14 18:01:58 -0300

    DisplayChat

--- scripts.js
+++ scripts.js
@@ -4,14 +4,15 @@
 //     }
 //     const promise = axios.get
 // }
+
 apiMessage()
-// setInterval(apiMessage,3000)
+sendUserName()
+setInterval(apiMessage,3000)
 
 
 function apiMessage (){
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
     promise.then(loadMessage);
-
 }
 function loadMessage (success){
     let data = {
@@ -24,16 +25,45 @@ function loadMessage (success){
 
     console.log(success)
     console.log(success.data.length)
-    for (let i = 0; iMax = success.data.length;  i < iMax, i++ ){
+    document.querySelector('.container').innerHTML = ""
+    for (let i = 0;  i < success.data.length; i++ ){
         data = {
-            from: success.data[i].from,
+            from:success.data[i].from,
             text:success.data[i].text,
             to:success.data[i].to,
             type:success.data[i].type,
             time:success.data[i].time,
         }
         document.querySelector('.container').innerHTML += `<div class="mensagem ${data.type}">
-        <p>(${data.time}) <strong>${data.from}</strong> para <strong>${data.to}</strong>: ${data.text} </p>
+        <p><span>(${data.time})</span> <strong> ${data.from}</strong> para <strong>${data.to}</strong>: ${data.text} </p>
         </div>`
+        
+    } 
+    console.log('b')
+    document.querySelector('.container div:last-child').scrollIntoView()
+    console.log('a')
+}
+  
+// function apiUsers (){
+//     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+//     promise.then()
+// }
+function sendUserName(){
+    const userName = prompt('Qual o seu nome?')
+    let user ={
+        name: userName
     }
+    const sendUser = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user)
+    sendUser.catch(userAlreadyTaken)
+}
+
+function userAlreadyTaken(erro) {
+	alert('Nome Já escolhido')
+	sendUserName()
+}
+
+
+
+function displayHidden(){
+    document.querySelector('.messageInterface').classList.remove('hidden')
 }
